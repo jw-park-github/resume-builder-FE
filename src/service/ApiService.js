231c897ba4a0bc63879247738d324c12bca1826d
jw-park-github <jw.park.github@gmail.com>
2024-07-25 16:32:20 +0900

    Initial commit

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -4,11 +4,11 @@ const ACCESS_TOKEN = "ACCESS_TOKEN";
 // API 호출 함수
 export function call(api, method, request, authRequired = true) {
   let headers = new Headers({
-    "Content-Type": "application/json", // Request 헤더 설정
+    "Content-Type": "application/json", // 요청 헤더 설정
   });
 
   if (authRequired) {
-    // local storage에서 ACCESS TOKEN 가져오기
+    // 로컬 스토리지에서 ACCESS TOKEN 가져오기
     const accessToken = localStorage.getItem(ACCESS_TOKEN);
     console.log("Access Token from localStorage:", accessToken);
 
@@ -31,20 +31,17 @@ export function call(api, method, request, authRequired = true) {
 
   return fetch(options.url, options)
       .then((response) => {
+
         return response.text().then((text) => {
-          console.log("Response Text:", text);
+          console.log("Response Text:", text); // 응답 텍스트 로그
           if (!response.ok) {
-            if (response.status === 403) {
-              alert("잘못된 접근입니다.");
-              window.location.href = "/resumes";
-            }
             return Promise.reject(text);
           }
           return text ? JSON.parse(text) : {}; // JSON 응답이 비어있을 경우 처리
         });
       })
       .catch((error) => {
-        console.error("Fetch error:", error);
+        console.error("Fetch error:", error); // 에러 로그
         if (error.status === 403) {
           window.location.href = "/login";
         }
@@ -56,19 +53,19 @@ export function call(api, method, request, authRequired = true) {
 export function signin(userDTO) {
   return call("/auth/signin", "POST", userDTO).then((response) => {
     if (response.token) {
-      localStorage.setItem(ACCESS_TOKEN, response.token); // 토큰을 local storage에 저장
-      window.location.href = "/resumes";
+      localStorage.setItem(ACCESS_TOKEN, response.token); // 토큰을 로컬 스토리지에 저장
+      window.location.href = "/resumes"; // 이력서 생성 페이지로 리디렉트
     }
   });
 }
 
 // 로그아웃 처리 함수
 export function signout() {
-  localStorage.removeItem(ACCESS_TOKEN); // 토큰 삭제
-  window.location.href = "/login";
+  localStorage.setItem(ACCESS_TOKEN, null); // 토큰 삭제
+  window.location.href = "/login"; // 로그인 페이지로 리디렉트
 }
 
 // 회원가입 처리 함수
 export function signup(userDTO) {
-  return call("/auth/signup", "POST", userDTO, false);
+  return call("/auth/signup", "POST", userDTO, false); // 회원가입 요청 시 authRequired를 false로 설정
 }
